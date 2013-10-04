785da7568ee43b31ed3f0523e2d43f703a92c316
Andrew Kofink <ajkofink@gmail.com>
2013-10-03 23:43:21 -0400

    Update jQuery and javascripts

--- config/application.rb
+++ config/application.rb
@@ -2,15 +2,15 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require *Rails.groups(:assets => %w(development test))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
-end
-
 module Expertiza
   class Application < Rails::Application
+    #if defined?(Bundler)
+      ## If you precompile assets before deploying to production, use this line
+      #Bundler.require *Rails.groups(:assets => %w(development test))
+      ## If you want your assets lazily compiled in production, use this line
+      ## Bundler.require(:default, :assets, Rails.env)
+    #end
+
     # Enable the asset pipeline
     config.assets.enabled = true
 
